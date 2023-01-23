4e4465ae937a1055b9daac095800e39bf77c9320
fdsc <fdsc@yandex.ru>
2023-01-23 04:33:21 +0300

    Вроде бы, новая версия программы работает

--- Program.cs
+++ Program.cs
@@ -20,31 +20,59 @@ class MainClass
         return result;
     }
     
-    static string[] captureStrs = new string[] { "MemTotal:", "MemAvailable:", "SwapTotal:" };
+    static string[] captureStrs = new string[] { "MemTotal:", "MemAvailable:", "SwapTotal:", "SwapFree:" };
     static Random   rnd         = new Random();
-    
-    const long      minAvailableMemory = 1024*1024 * 3/2; // in kilobytes 1024*1024 = 1 Gb
+
+    const string    errorFileName      = "error.log";
     const int       intervalForCheck   = 500;
     public static int Main(string[] args)
     {
-        if (args.Length != 1 && args.Length != 2)
+        if (args.Length != 1)
         {
-            Console.Error.WriteLine("Must be 1 or 2 arguments: script path for execute and script parameters in quotes");
+            Console.Error.WriteLine("Must be 1 argument: options file path");
+            Console.Error.WriteLine("If options file not exists, it will be created");
             Console.Error.WriteLine("Example");
-            Console.Error.WriteLine("/media/veracrypt1/swap.sh \"2G\"");
+            Console.Error.WriteLine("enswap");
 
             return 1;
             // args = new string[] { "/media/veracrypt1/swap.sh", "2G" };
         }
 
+        var optionsFileName = new FileInfo(args[0]);
+        optionsFileName.Refresh();
+        if (!optionsFileName.Exists)
+        {
+            File.WriteAllText(optionsFileName.FullName, "4G\n1536\n/Arcs/swapfile\n/G/sdel\n");
+            Console.Error.WriteLine("Options file created. See it and let's correct");
+
+            return 2;
+        }
+
+        Console.WriteLine($"strating with options file '{optionsFileName.FullName}'");
+
+        var optionsStrings = File.ReadAllLines(optionsFileName.FullName);
+        var swapL = optionsStrings[0];
+        var freeM = long.Parse(optionsStrings[1]) * 1024;
+        var swapF = optionsStrings[2];
+        var sdel  = optionsStrings[3];
+
+        deleteAllSwaps(swapF, sdel);
+
         var exec = true;
         var Dict = new Dictionary<string, long>(2);
         Console.CancelKeyPress += (sender, e) => exec = false;
+        AppDomain.CurrentDomain.ProcessExit += (sender, e) => exec = false;
+
+        Console.WriteLine($"strated. '{swapL}'; '{freeM}'; '{swapF}'; '{sdel}'.");
+
+        bool outputStatisticToConsole = false;
 
         while (exec)
         {
             Dict.Clear();
-            Thread.Sleep(rnd.Next(50, intervalForCheck));
+            Thread.Sleep(rnd.Next(56, intervalForCheck));
+            if (!exec)
+                break;
 
             // System.Diagnostics.Process.Start("free");
             var memInfoLines = File.ReadAllLines("/proc/meminfo");
@@ -57,27 +85,128 @@ class MainClass
                         Dict.Add(captureStr, ParseValue(line));
                     }
 
-                if (Dict.Count >= 3)
+                if (Dict.Count >= captureStrs.Length)
                     break;
             }
 
-            if (Dict[captureStrs[1]] <= minAvailableMemory)
+            var totalFree = Dict[captureStrs[1]] + Dict[captureStrs[3]];
+            if (!outputStatisticToConsole)
             {
+                Console.WriteLine($"Statistic of memory: available {Dict[captureStrs[1]]}; {Dict[captureStrs[3]]}; {totalFree}; require {freeM}");
+                outputStatisticToConsole = true;
+            }
+
+            if (totalFree <= freeM)
+            {
+                Console.WriteLine($"Low memory: {Dict[captureStrs[1]]}; {Dict[captureStrs[3]]}; {totalFree} <= {freeM}");
                 // if (Dict[captureStrs[2]] <= 0)
+                var newSwapFile = getFile(swapF, swapL, sdel);
+                swapFiles.Add(newSwapFile);
 
-                Process pi;
-                if (args.Length == 1)
-                    pi = System.Diagnostics.Process.Start(args[0]);
-                else
-                    pi = System.Diagnostics.Process.Start(args[0], args[1]);
-
-                pi.WaitForExit();
-
-                return 0;
+                outputStatisticToConsole = false;
+                Thread.Sleep(intervalForCheck);
             }
         }
+
+        Console.WriteLine("exiting");
+        var pid = Process.Start("swapoff", "-a");
+        pid.WaitForExit();
+
+        deleteAllSwaps(swapF, sdel);
         
         return 0;
     }
-}
 
+    static void deleteAllSwaps(string pathTemplate, string sdel_path)
+    {
+        Process? pi;
+        for (int i = swapFiles.Count; i < 100; i++)
+        {
+            try
+            {
+                var fi = new FileInfo(pathTemplate + "-" + i.ToString("D2"));
+                fi.Refresh();
+                if (fi.Exists)
+                {
+                    Console.WriteLine($"try to delete file {fi.FullName}");
+                    pi = Process.Start(sdel_path, $"- {fi.FullName}");
+                    pi.WaitForExit();
+                }
+                else
+                    continue;
+
+                fi.Refresh();
+                if (fi.Exists)
+                {
+                    var err = $"swap files can not delete with template '{pathTemplate}' {DateTime.Now.ToString()}";
+                    Console.WriteLine(err);
+                    File.AppendAllText(errorFileName, err + "\n");
+                }
+                else
+                    Console.WriteLine($"file deleted: {fi.FullName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                File.AppendAllText(errorFileName, ex.Message + "\n");
+            }
+        }
+    }
+
+    static List<FileInfo> swapFiles = new List<FileInfo>();
+    static FileInfo getFile(string pathTemplate, string swapFileLenParam, string sdel_path)
+    {
+        Process? pi;
+        for (int i = swapFiles.Count; i < 3; i++)
+        {
+            var fi = new FileInfo(pathTemplate + "-" + i.ToString("D2"));
+            fi.Refresh();
+            try
+            {
+                if (fi.Exists)
+                {
+                    pi = Process.Start(sdel_path, $"- {fi.FullName}");
+                    pi.WaitForExit();
+                }
+
+                fi.Refresh();
+                if (fi.Exists)
+                    continue;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                File.AppendAllText(errorFileName, ex.Message + "\n");
+                continue;
+            }
+
+            pi = Process.Start("fallocate", $"-l {swapFileLenParam} {fi.FullName}");
+            pi.WaitForExit();
+
+            fi.Refresh();
+            if (!fi.Exists)
+            {
+                var errf = $"swap files can not created with template '{pathTemplate}' [fallocate problem] {DateTime.Now.ToString()}";
+                File.AppendAllText(errorFileName, errf + "\n");
+                throw new Exception(errf);
+            }
+
+            pi = Process.Start(fileName: "chmod", $"0600 {fi.FullName}");
+            pi.WaitForExit();
+
+            pi = Process.Start(fileName: "mkswap", $"{fi.FullName}");
+            pi.WaitForExit();
+
+            pi = Process.Start(fileName: "swapon", $"-v {fi.FullName}");
+            pi.WaitForExit();
+
+            Console.WriteLine($"Swap file created: {fi.FullName}");
+
+            return fi;
+        }
+
+        var err = $"swap files can not created with template '{pathTemplate}' {DateTime.Now.ToString()}";
+        File.AppendAllText(errorFileName, err + "\n");
+        throw new Exception(err);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # enswap
+For Linux require to install [.NET 7.0](https://dotnet.microsoft.com/download)
+
 swapfile expander linux service. If Linux has less than 1.5 GB available memory, it allocate new swap
+
+## build:
+git clone https://github.com/VinnySmallUtilities/enswap
+cd enswap
+dotnet publish -c Release --self-contained false /p:PublishSingleFile=true --output ./build/
+
+
+## Executing
+Let's correct the "enswap.service" file
+
+first executing:
+enswap options
+
+let's see the created "options" file and correct it
+first string:  parameter for file length for single swap file (format for "fallocate" program)
+second string: parameter for available memory (in Mb)
+third string:  a file path template for swap files
+fourth string: sdel utility path (https://github.com/VinnySmallUtilities/sdel)
+
+second executing:
+
+sudo cp -v enswap.service /lib/systemd/system/
+sudo systemctl enable enswap.service
+sudo systemctl daemon-reload
+sudo systemctl start enswap.service
+
